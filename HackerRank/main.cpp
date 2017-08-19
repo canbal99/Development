059abb670bf60a6bdced24256014f4fba7ce8621
Can Canbolat <can.canbolat@argela.com.tr>
2017-08-19 11:36:55 +0200

    Day 27: Testing

--- HackerRank/main.cpp
+++ HackerRank/main.cpp
@@ -1,25 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: can
- *
- * Created on August 18, 2017, 10:12 AM
- */
-
-#include <cstdlib>
+#include <iostream>
+#include <set>
+#include <stdio.h>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#define RANDOM(min,max)     (rand()%(max-min+1) + min)
 
+void printArrivalTimes(int n, int k, bool canceled) {
+    for (int l = 0; l < n - 2; l++) {
+        if (!canceled)
+            cout << RANDOM(-1000,-1) << ' ';
+        else
+            cout << RANDOM(1,1000) << ' ';
+    }
+    if (!canceled)
+        cout << "0 1" << endl;
+    else
+        cout << "0 -1" << endl;
+}
+int main(int argc, char** argv) {
+    srand(time(NULL));
+    set<int> nValues;
+    int t, n, k, a, i, j, l;
+    for (i = 0; i < 1; i++) {
+        t = 5;//RANDOM(1,5);
+        cout << t << endl;
+        for (j = 0; j < t; j++) {
+            do { // The value of  must be distinct across all the test cases.
+                n = RANDOM(3,200);
+            } while (!nValues.insert(n).second);
+            k = RANDOM(1,n);
+            cout << n << ' ' << k << endl;
+            printArrivalTimes(n,k,j%2==0);
+        }
+    }
+        
     return 0;
 }
 
