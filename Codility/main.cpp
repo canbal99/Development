7eb7aace392f5e77296b5844e131d3239d78eb80
Can Canbolat <can.canbolat@virtualads.com>
2017-08-21 15:45:53 +0200

    Codility Manganum 2017 Contest - UPDATED

--- Codility/main.cpp
+++ Codility/main.cpp
@@ -7,20 +7,28 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <set>
+#include <cmath>
 
 using namespace std;
 
 //#define __TEST__
-#define MAX(x,y)     ((x)>(y)?(x):(y))
+#define RANDOM(min,max)     (rand()%(max-min+1) + min)
+#define MAX(x,y)            ((x)>(y)?(x):(y))
+#define ALL_DIRECTIONS      0
+#define START_MEASURE()     clock_t begin = clock();
+#define PRINT_MEASURE()     clock_t end = clock();double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;cout<<"Time elapsed:"<<elapsed_secs<<endl;printf("Time elapsed:%lf\n",elapsed_secs);
+#define cout                (ofs==NULL?cout:*ofs)
+ofstream *ofs = NULL;
 
 struct Piece {
     int x;
     int y;
     int value;
-    bool alreadyBeaten;
+    int alreadyBeaten;
     
-    Piece() : x(0), y(0), value(0), alreadyBeaten(false) {};
+    Piece() : x(0), y(0), value(0), alreadyBeaten(0) {};
     
     bool isEqual(const Piece& piece) const {
         return piece.x==x && piece.y==y;
@@ -74,6 +82,48 @@ void printSet(set<Piece,T>& axis, string title="") {
     }
 }
 
+template <class T>
+void drawSetOnBoard(set<Piece,T>& axis, Piece& piece, string title="") {
+    if (title.length()>0) cout << title << endl;
+    
+    int maxValue = 0;
+    maxValue = MAX(maxValue,abs(piece.x));
+    maxValue = MAX(maxValue,abs(piece.y));
+    for (auto item : axis) {
+        maxValue = MAX(maxValue,abs(item.x));
+        maxValue = MAX(maxValue,abs(item.y));
+    }
+    cout << "MaxValue:" << maxValue << endl;
+    
+    vector< vector<char> > board;
+    board.resize(2*maxValue+1);
+    for (int i=0;i<board.size();i++) {
+        board[i].resize(board.size(), '.');
+    }
+    cout << "Board initilaized:" << board.size() << "x" << board[0].size() << endl;
+    
+    cout << "My piece on " << piece.y << "," << piece.x << endl;
+    board[maxValue-piece.y][piece.x+maxValue] = 'X';
+    for (auto item : axis) {
+        cout << "Item on " << item.y << "," << item.x << endl;
+        board[maxValue-item.y][item.x+maxValue] = item.value==1?'p':'q';
+    }
+    cout << "Items drawn on vector" << endl;
+    
+    for (int y=-1*maxValue;y<=maxValue;y++) {cout << "##";}cout << endl;
+    cout << "  ";for (int y=-1*maxValue;y<=maxValue;y++) {if (abs(y)<10) cout << ' ';cout << abs(y);}cout << endl;
+    for (int y=-1*maxValue;y<=maxValue;y++) {
+        if (abs(y)<10) cout << ' ';cout << abs(y);
+        for (int x=-1*maxValue;x<=maxValue;x++) {
+            cout << ' ' << board[y+maxValue][x+maxValue];
+        }
+        if (abs(y)<10) cout << ' ';cout << abs(y);
+        cout << endl;
+    }
+    cout << "  ";for (int y=-1*maxValue;y<=maxValue;y++) {if (abs(y)<10) cout << ' ';cout << abs(y);}cout << endl;
+    for (int y=-1*maxValue;y<=maxValue;y++) cout << "##";cout << endl;
+}
+
 class GameBoard {
 public:
     GameBoard(vector<int> &X, vector<int> &Y, string &T) {
@@ -83,7 +133,7 @@ public:
             temp.y = Y[i];
             if (T[i]=='X') temp.value = 0;
             else temp.value = (T[i]=='p'?1:10);
-            if (temp.value==0) myPiece = temp;
+            if (temp.value==0 || temp.isEqual(myPiece)) myPiece = temp;
             else insertPieceToSets(temp);
         }
         
@@ -91,6 +141,7 @@ public:
         printPiece(myPiece, "MyPiece:");
         printSet<PositiveAxis>(positiveAxis, "Positive:");
         printSet<NegativeAxis>(negativeAxis, "Negative:");
+        drawSetOnBoard<PositiveAxis>(positiveAxis, myPiece);
 #endif
     }
     
@@ -101,8 +152,8 @@ public:
 private:
     int findMaxValue(const Piece& startFromPiece) {
         int valueOnPosForw = findMaxValue(startFromPiece, true, true);
-        int valueOnPosBack = 0; // findMaxValue(startFromPiece, false, true);
-        int valueOnNegForw = 0; // findMaxValue(startFromPiece, true, false);
+        int valueOnPosBack = ALL_DIRECTIONS ? findMaxValue(startFromPiece, false, true) : 0;
+        int valueOnNegForw = ALL_DIRECTIONS ? findMaxValue(startFromPiece, true, false) : 0;
         int valueOnNegBack = findMaxValue(startFromPiece, false, false);
         return MAX(MAX(valueOnPosForw,valueOnPosBack),MAX(valueOnNegForw,valueOnNegBack));
     }
@@ -117,6 +168,10 @@ private:
         printSet<PositiveAxis>(positiveAxis, "Positive:");
         printSet<NegativeAxis>(negativeAxis, "Negative:");
 #endif
+        //if (ALL_DIRECTIONS==0) {
+        //    hideAllUnnecesaryItems(startFromPiece);
+        //}
+        
         auto nextItemOnPos = POSITIVE ? positiveAxis.upper_bound(startFromPiece) : negativeAxis.upper_bound(startFromPiece);
         if (!FORWARD) iterate(&nextItemOnPos, FORWARD, POSITIVE);
         
@@ -182,6 +237,7 @@ private:
                 if (value>0 && !findMaxValueCalledForTempPos) {
                     tempPos = startFromPiece.findNextPositionAfterBeating(*nextItemOnPos);
                     max = findMaxValue(tempPos);
+                    findMaxValueCalledForTempPos = true;
                 }
                 value += max;
                 insertPieceToSets(*nextItemOnPos);
@@ -196,35 +252,40 @@ private:
 #ifdef __TEST__
         cout << "<<< returning " << FORWARD << " " << POSITIVE << " value:" << value << endl;
 #endif
+        //if (ALL_DIRECTIONS==0) {
+        //    unhideAllUnnecesaryItems(startFromPiece);
+        //}
         return value;
     }
     
     void iterate(void *iterator, bool FORWARD, bool POSITIVE) {
-        do {
-            iterate_old(iterator, FORWARD, POSITIVE);
-        } while (!checkIfEndOfIteration(iterator, FORWARD, POSITIVE) && alreadyBeaten(iterator, FORWARD, POSITIVE));
-    }
-    
-    void iterate_old(void *iterator, bool FORWARD, bool POSITIVE) {
         if (POSITIVE) {
             set<Piece, PositiveAxis>::iterator &iter(*(set<Piece, PositiveAxis>::iterator*)(iterator));
             if (FORWARD) {
-                if (iter != positiveAxis.end())
+                do {
                     iter++;
-            } else if (iter==positiveAxis.begin()) {
-                iter = positiveAxis.end();
+                } while (iter != positiveAxis.end() && (*iter).alreadyBeaten);
             } else {
-                iter--;
+                do {
+                if (iter==positiveAxis.begin())
+                    iter = positiveAxis.end();
+                else
+                    iter--;
+                } while (iter != positiveAxis.end() && (*iter).alreadyBeaten);
             }
         } else {
             set<Piece, NegativeAxis>::iterator &iter(*(set<Piece, NegativeAxis>::iterator*)(iterator));
             if (FORWARD) {
-                if (iter != negativeAxis.end())
+                do {
                     iter++;
-            } else if (iter==negativeAxis.begin()) {
-                iter = negativeAxis.end();
+                } while (iter != negativeAxis.end() && (*iter).alreadyBeaten);
             } else {
-                iter--;
+                do {
+                if (iter==negativeAxis.begin())
+                    iter = negativeAxis.end();
+                else
+                    iter--;
+                } while (iter != negativeAxis.end() && (*iter).alreadyBeaten);
             }
         }
     }
@@ -250,11 +311,11 @@ private:
 #endif
         auto positive = positiveAxis.find(piece);
         if (positive!=positiveAxis.end()) {
-            ((Piece&)(*positive)).alreadyBeaten = true;
+            ((Piece&)(*positive)).alreadyBeaten++;
         }
         auto negative = negativeAxis.find(piece);
         if (negative!=negativeAxis.end()) {
-            ((Piece&)(*negative)).alreadyBeaten = true;
+            ((Piece&)(*negative)).alreadyBeaten++;
         }
     }
     
@@ -264,26 +325,55 @@ private:
 #endif
         auto positive = positiveAxis.find(piece);
         if (positive!=positiveAxis.end()) {
-            ((Piece&)(*positive)).alreadyBeaten = false;
+            if (((Piece&)(*positive)).alreadyBeaten>0)
+                ((Piece&)(*positive)).alreadyBeaten--;
         } else {
             positiveAxis.insert(piece);
         }
         
         auto negative = negativeAxis.find(piece);
         if (negative!=negativeAxis.end()) {
-            ((Piece&)(*negative)).alreadyBeaten = false;
+            if (((Piece&)(*negative)).alreadyBeaten>0)
+                ((Piece&)(*negative)).alreadyBeaten--;
         } else {
             negativeAxis.insert(piece);
         }
     }
     
-    bool alreadyBeaten(void *iterator, bool FORWARD, bool POSITIVE) {
-        if (POSITIVE) {
-            set<Piece, PositiveAxis>::iterator &iter(*(set<Piece, PositiveAxis>::iterator*)(iterator));
-            return (*iter).alreadyBeaten;
-        } else {
-            set<Piece, NegativeAxis>::iterator &iter(*(set<Piece, NegativeAxis>::iterator*)(iterator));
-            return (*iter).alreadyBeaten;
+    void hideAllUnnecesaryItems(const Piece& startFromPiece) {
+        int relX, relY;
+        for (auto item : positiveAxis) {
+            relX = item.x-startFromPiece.x;
+            relY = item.y-startFromPiece.y;
+            if (relX-relY<0 || -relX-relY<0) {
+                item.alreadyBeaten++;
+            }
+        }
+        for (auto item : negativeAxis) {
+            relX = item.x-startFromPiece.x;
+            relY = item.y-startFromPiece.y;
+            if (relX-relY<0 || -relX-relY<0) {
+                item.alreadyBeaten++;
+            }
+        }
+    }
+    
+    void unhideAllUnnecesaryItems(const Piece& startFromPiece) {
+        return;
+        int relX, relY;
+        for (auto item : positiveAxis) {
+            relX = item.x-startFromPiece.x;
+            relY = item.y-startFromPiece.y;
+            if (relX-relY<0 || -relX-relY<0) {
+                item.alreadyBeaten--;
+            }
+        }
+        for (auto item : negativeAxis) {
+            relX = item.x-startFromPiece.x;
+            relY = item.y-startFromPiece.y;
+            if (relX-relY<0 || -relX-relY<0) {
+                item.alreadyBeaten++;
+            }
         }
     }
     
@@ -302,6 +392,7 @@ int solution(vector<int> &X, vector<int> &Y, string &T) {
 int main(int argc, char** argv) {
 
     do {
+    
     vector<int> X1 = {3, 5, 1, 6};
     vector<int> Y1 = {1, 3, 3, 8};
     string T1 = "Xpqp";
@@ -335,6 +426,38 @@ int main(int argc, char** argv) {
     if (result4!=11) break;
     
     } while (false);
+    
+    
+    int count;
+    int run_count = 100;
+    double average_time = 0;
+    #define ELEMENT_COUNT       200
+
+    for (int i=0;i<run_count;i++) {
+    ofstream logFile("/Users/can/Desktop/test.txt", std::ofstream::out);
+    ofs = &logFile;
+    srand(clock());
+    vector<int> X(ELEMENT_COUNT);
+    vector<int> Y(ELEMENT_COUNT);
+    string T;
+    const int random_interval = sqrt(3*ELEMENT_COUNT);
+    for (int i=0;i<ELEMENT_COUNT;i++) {
+        X[i] = RANDOM(-random_interval, random_interval);
+        Y[i] = RANDOM(-random_interval, random_interval);
+        T.push_back((X[i]+Y[i])%2==0?'p':'q');
+    }
+    T[RANDOM(0, ELEMENT_COUNT-1)] = 'X';
+    cout << "Answer for " << ELEMENT_COUNT << " different elements" << endl;
+    START_MEASURE();
+    int result = solution(X,Y,T);
+    PRINT_MEASURE();
+    cout << "RESULT: " << result << endl;
+    logFile.close();
+    average_time += elapsed_secs;
+    }
+    
+    cout<<"Average time elapsed:"<<average_time/run_count<<endl;
+    printf("Average time elapsed:%lf\n",average_time/run_count);
             
     return 0;
 }
