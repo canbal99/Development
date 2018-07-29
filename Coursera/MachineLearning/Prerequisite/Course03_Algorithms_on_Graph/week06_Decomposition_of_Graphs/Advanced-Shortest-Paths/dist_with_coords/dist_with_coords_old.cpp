dda3d271500f0f1f50b324faa3f0af9b37433cdc
can <can_bal99@hotmail.com>
2018-07-29 23:27:59 +0200

    updated

--- Coursera/MachineLearning/Prerequisite/Course03_Algorithms_on_Graph/week06_Decomposition_of_Graphs/Advanced-Shortest-Paths/dist_with_coords/dist_with_coords_old.cpp
+++ Coursera/MachineLearning/Prerequisite/Course03_Algorithms_on_Graph/week06_Decomposition_of_Graphs/Advanced-Shortest-Paths/dist_with_coords/dist_with_coords_old.cpp
@@ -10,10 +10,10 @@
 #include <unordered_map>
 
 using namespace std;
-using std::cout;
 
 const int F = 0;
 const int R = 1;
+#define DISP    "%lld" //"%.7f"
 
 // See the explanations of these typedefs and constants in the starter for friend_suggestion
 typedef vector<vector<vector<int>>> Adj;
@@ -25,58 +25,46 @@ const Len INFINITY_ = numeric_limits<Len>::max() / 4;
 class AStar {
     // See the descriptions of these fields in the starter for friend_suggestion
     int n_;
-    const Adj &adj_;
-    const Adj &cost_;
+    Adj adj_;
+    Adj cost_;
     vector<vector<Len>> distance_;
-    vector<int> workset_;
+    unordered_set<int> workset_;
     //vector<bool> visited_;
     vector<unordered_set<int>> processedset_;
     // Coordinates of the nodes
     std::vector<std::pair<Len,Len>> xy_;
+    unordered_map<int,Len> potential_;
+    vector<vector<int>> prev_;int pathVertex_;
+    vector<int> destination_;
     bool usePotential_;
-    int source,target;
 
 public:
-    AStar(int n, Adj& adj, Adj& cost, std::vector<std::pair<Len,Len>> xy, bool usePotential=true)
-        : n_(n), adj_(adj), cost_(cost), distance_(2, vector<Len>(n_, INFINITY_)), processedset_(2,unordered_set<int>(n)), xy_(xy),
-                usePotential_(usePotential)
-        { workset_.reserve(n); processedset_[F].reserve(n); processedset_[R].reserve(n); }
+    AStar(int n, Adj adj, Adj cost, std::vector<std::pair<Len,Len>> xy, bool usePotential=true)
+        : n_(n), adj_(adj), cost_(cost), distance_(2, vector<Len>(n_, INFINITY_)), processedset_(2,unordered_set<int>(n)), xy_(xy), prev_(2,vector<int>(n)), usePotential_(usePotential), pathVertex_(-1)
+    { }
 
     // See the description of this method in the starter for friend_suggestion
     void clear() {
+        pathVertex_ = -1;
         for (int v : workset_) {
             distance_[0][v] = distance_[1][v] = INFINITY_;
+            prev_[0][v] = prev_[1][v] = -1;
         }
         workset_.clear();
         processedset_[F].clear();
         processedset_[R].clear();
+        destination_.clear();
+        potential_.clear();
     }
 
     // See the description of this method in the starter for friend_suggestion
-
-    void visit(Queue& q, int side, int v, Len dist) {
+    void visit(Queue& q, int side, int v, int prev, Len dist) {
         // Implement this method yourself
-        workset_.push_back(v);
+        workset_.insert(v);
         distance_[side][v] = dist;
-        q[side].push(make_pair(dist, v));
+        prev_[side][v] = prev;
+        q[side].push(make_pair(dist /*- getPotential(side, v) + getPotential(side, prev)*/, v));
         //cout << "(visited:" << distance_[side][v] << ")";
-    }
-    
-    inline Len getDistance(int p1, int p2) {
-        Len xDiff = xy_[p1].first-xy_[p2].first;
-        Len yDiff = xy_[p1].second-xy_[p2].second;
-        return sqrt(xDiff*xDiff+yDiff*yDiff);
-    }
-    
-    Len getPotential(int side, int v) {
-        if (!usePotential_)
-            return 0;
-        //if (potential_.count(v)==0) {
-            int potential = (getDistance(v,target) - getDistance(v,source)) / 2;
-        //    potential_[v] = potential;
-            return (side ? -1*potential : potential);
-        //}
-        //return (side?-1:1)*potential_[v];
     }
     
     void process(Queue& q, int side, int v) {
@@ -89,9 +77,9 @@ public:
         for (int i=adj_[side][v].size()-1; i>=0; i--) {
             adj = adj_[side][v][i];
             newDist = dist + cost_[side][v][i] - getPotential(side, v) + getPotential(side, adj);
-            //cout << " " << adj+1 << "(dist:" << (distance_[side][adj]==INFINITY_?"INF":to_string(distance_[side][adj])) << "-cost:" << cost << ")";
+            //cout << " " << adj+1 << "(dist:" << (distance_[side][adj]==INFINITY_?"INF":to_string(distance_[side][adj])) << "-cost:" << cost_[side][v][i] << ")";
             if (distance_[side][adj] > newDist) {
-                visit(q, side, adj, newDist);
+                visit(q, side, adj, v, newDist);
             }
         }
         //cout<<"\n";
@@ -102,26 +90,44 @@ public:
         Len candidate;
         for (int v : processedset_[F]) {
             candidate = distance_[F][v] + distance_[R][v];
-            if (minimum > candidate) minimum = candidate;
+            if (minimum > candidate) { minimum = candidate; pathVertex_ = v;}
         }
         for (int v : processedset_[R]) {
             candidate = distance_[F][v] + distance_[R][v];
-            if (minimum > candidate) minimum = candidate;
+            if (minimum > candidate) { minimum = candidate; pathVertex_ = v;}
         }
-        return minimum + 2 * getPotential(F,source);
+        //return minimum + 2 * getPotential(F, destination_[R]) - getPotential(F, pathVertex_) - getPotential(R, pathVertex_);
+        return minimum + 2 * getPotential(F,destination_[R]);
+    }
+    
+    Len getDistance(int p1, int p2) {
+        return sqrt(pow(xy_[p1].first-xy_[p2].first,2)+pow(xy_[p1].second-xy_[p2].second,2));
+    }
+    
+    Len getPotential(int side, int v) {
+        if (!usePotential_)
+            return 0;
+        if (potential_.count(v)==0) {
+            Len potential = (getDistance(v,destination_[F]) - getDistance(v,destination_[R])) / 2;
+            potential_[v] = potential;
+            //cout << v << " pi " << potential << "->" << getDistance(v,destination_[F]) << "-" << getDistance(v,destination_[R]) << "\n";
+        }
+        return (side?-1:1)*potential_[v];
     }
 
     // Returns the distance from s to t in the graph
     Len query(int s, int t) {
-        source = s;
-        target = t;
+        
         if (s==t) return 0;
+        destination_.push_back(t);
+        destination_.push_back(s);
+        
         clear();
         Queue q(2);
-        visit(q, 0, s, 0);
-        visit(q, 1, t, 0);
+        visit(q, 0, s, s, 0);
+        visit(q, 1, t, t, 0);
         // Implement the rest of the algorithm yourself
-
+        
         int f,r;
         while (!q[F].empty() || !q[R].empty()) {
             
@@ -150,15 +156,154 @@ public:
 
         return -1;
     }
-    int getProcessedCount() {
-        return processedset_[F].size() + processedset_[R].size();
-    }
+    
     int getWorksetCount() {
         return workset_.size();
     }
+    
+    int getProcessedCount() {
+        return processedset_[F].size() + processedset_[R].size();
+    }
+    
+    vector<int> getPath() {
+        vector<int> path;
+        if (pathVertex_==-1) return path;
+        int FR;
+        
+        int f = pathVertex_;
+        while (f!=destination_[R]) {
+            path.insert(path.begin(), f);
+            f = prev_[F][f];
+        }
+        path.insert(path.begin(), destination_[R]);
+        
+        int r = pathVertex_;
+        while (r!=destination_[F]) {
+            r = prev_[R][r];
+            path.push_back(r);
+        }
+        
+        /*printf("LOG1:%d:" DISP "-" DISP "\n",pathVertex_,getPotential(F,pathVertex_),getPotential(R,pathVertex_));
+        FR = F;
+        for (int i=0; i<path.size();i++) {
+            Len pot = getPotential(FR,path[i]);
+            cout << path[i] << "(" << pot << ")" << "->";
+            if (path[i]==pathVertex_ && FR==F) {FR = R;i--;}
+        }
+        cout << "\nLOG2:";
+        FR = F;
+        for (int i=0; i<path.size()-1;i++) {
+            Len pot = getPotential(FR,path[i]) - getPotential(FR,path[i+1]);
+            cout << path[i] << "->(" << pot << ")" << "->";
+            if (path[i]==pathVertex_ && FR==F) {FR = R;i--;}
+        }
+        cout << "\nLOG3:";
+        FR = F;
+        for (int i=0; i<path.size();i++) {
+            Len dist = distance_[FR][path[i]];
+            Len cost = 0;
+            for (int j=0;i<path.size()-1 && j<adj_[F][path[i]].size();j++) {
+                if (adj_[F][path[i]][j] == path[i+1]) {
+                    cost = cost_[F][path[i]][j];
+                    break;
+                }
+            }
+            printf("%d(" DISP "-" DISP ")->",path[i],dist,cost);
+            if (path[i]==pathVertex_ && FR==F) {FR = R;i--;}
+        }
+        cout << "\n";*/
+        
+        return path;
+    }
 };
+/*
+#define SIDE                50
+#define COUNT               (SIDE*SIDE)
+#define POS_(x,y)           ((y)*SIDE+(x))
+#define POS                 POS_(x,y)
+#define COST                (20 + (rand()%1000))
 
+void addConn(Adj& adj, Adj& cost, int pos, int adjPos) {
+    if (adjPos<0 || adjPos>=COUNT) return;
+    if (pos+1==adjPos && adjPos%SIDE==0) return;
+    if (pos==adjPos+1 && pos%SIDE==0) return;
+    
+    int c = COST;
+    adj[F][pos].push_back(adjPos);
+    cost[F][pos].push_back(c);
+    adj[R][adjPos].push_back(pos);
+    cost[R][adjPos].push_back(c);
+}
+
+void test_() {
+    
+    int test_count = 0;
+    while (true) {
+        std::vector<std::pair<Len,Len>> xy(COUNT);
+        Adj adj(2, vector<vector<int>>(COUNT));
+        Adj cost(2, vector<vector<int>>(COUNT));
+
+        for (int y=0; y<SIDE; y++) {
+            for (int x=0; x<SIDE; x++) {
+                xy[POS] = make_pair(x*10,y*10);
+                addConn(adj, cost, POS, POS_(x,y-1));
+                addConn(adj, cost, POS, POS_(x,y+1));
+                addConn(adj, cost, POS, POS_(x-1,y));
+                addConn(adj, cost, POS, POS_(x+1,y));
+            }
+        }
+    
+        vector<int> path;
+        AStar astar(COUNT, adj, cost, xy);
+        AStar dijks(COUNT, adj, cost, xy, false);
+        
+        //int source = 0;
+        //int dest = SIDE*(SIDE-1);
+        int source = rand()%COUNT;
+        int dest = rand()%COUNT;
+        
+        cout << test_count++ << " source:" << source << " destination:" << dest << "\n";
+        Len resAStar = astar.query(source,dest);
+        Len resDijks = dijks.query(source,dest);
+        //cout << "sample cost:" << COST << "\n";
+        cout << "astar:" << resAStar << " processed:" << astar.getProcessedCount() << " workset:" << astar.getWorksetCount() << "\n";
+        cout << "dijks:" << resDijks << " processed:" << dijks.getProcessedCount() << " workset:" << dijks.getWorksetCount() << "\n";
+        
+        if (resAStar != resDijks) {
+            printf(DISP "==" DISP "\n", resAStar, resDijks);
+        
+            path = astar.getPath();
+            std::for_each(path.begin(), path.end(), [](int& elem) {cout << elem << "->";});cout << "\n";
+
+            path = dijks.getPath();
+            std::for_each(path.begin(), path.end(), [](int& elem) {cout << elem << "->";});cout << "\n";
+            
+            for (int i = 0; i < COUNT; i++) {
+                cout << "\nPoint:" << i << "(" << xy[i].first << "," << xy[i].second << ")" << " adj:";
+                for (int j = 0; j < adj[F][i].size(); j++) {
+                    cout << " " << adj[F][i][j] << "(" << cost[F][i][j] << ")";
+                }
+            }
+            cout << "\n";
+            for (int i = 0; i < COUNT; i++) {
+                cout << "\nPoint:" << i << "(" << xy[i].first << "," << xy[i].second << ")" << " adj:";
+                for (int j = 0; j < adj[R][i].size(); j++) {
+                    cout << " " << adj[R][i][j] << "(" << cost[R][i][j] << ")";
+                }
+            }
+            cout << "\n";
+
+            break;
+        }
+        
+        //cout << "\n\n\n\n\n\n";
+        //getchar();
+    }
+}
+*/
 int main() {
+    
+    //test_();
     
     int n, m;
     scanf("%d%d", &n, &m);
@@ -186,126 +331,6 @@ int main() {
     for (int i=0; i<t; ++i) {
         int u, v;
         scanf("%d%d", &u, &v);
-        printf("%lld\n", astar.query(u-1, v-1));
+        printf(DISP "\n", astar.query(u-1, v-1));
     }
 }
-
-
-
-/*
-
-class TimeElapsed {
-    
-    clock_t begin;
-    clock_t end;
-
-public:    
-    TimeElapsed() {start();};
-    void start() {begin = clock();}
-    void stop(string title="")  {
-        end = clock();
-        int msec = double(end - begin) / 1000;
-        cout << "TimeElapsed for <" << title << "> is " << msec << " ms\n";
-    }
-};
-
-#define DISP                "%lld"
-#define SIDE                500
-#define COUNT               (SIDE*SIDE)
-#define POS_(x,y)           ((y)*SIDE+(x))
-#define POS                 POS_(x,y)
-#define COST                10//(20 + (rand()%1000))
-
-void addConn(Adj& adj, Adj& cost, int pos, int adjPos) {
-    if (adjPos<0 || adjPos>=COUNT) return;
-    if (pos+1==adjPos && adjPos%SIDE==0) return;
-    if (pos==adjPos+1 && pos%SIDE==0) return;
-    
-    int c = COST;
-    adj[F][pos].push_back(adjPos);
-    cost[F][pos].push_back(c);
-    adj[R][adjPos].push_back(pos);
-    cost[R][adjPos].push_back(c);
-}
-
-void test_() {
-    
-    return;
-    
-    TimeElapsed elapsed;
-    int test_count = 0;
-    while (true) {
-        std::vector<std::pair<Len,Len>> xy(COUNT);
-        Adj adj(2, vector<vector<int>>(COUNT));
-        Adj cost(2, vector<vector<int>>(COUNT));
-
-        for (int y=0; y<SIDE; y++) {
-            for (int x=0; x<SIDE; x++) {
-                xy[POS] = make_pair(x*10,y*10);
-                addConn(adj, cost, POS, POS_(x,y-1));
-                addConn(adj, cost, POS, POS_(x,y+1));
-                addConn(adj, cost, POS, POS_(x-1,y));
-                addConn(adj, cost, POS, POS_(x+1,y));
-            }
-        }
-    
-        vector<int> path;
-        elapsed.start();
-        AStar astar(COUNT, adj, cost, xy);
-        elapsed.stop("AStar init");
-        elapsed.start();
-        AStar dijks(COUNT, adj, cost, xy, false);
-        elapsed.stop("Dijkstra init");
-        
-        //int source = 0;
-        //int dest = SIDE*(SIDE-1);
-        int source = rand()%COUNT;
-        int dest = rand()%COUNT;
-        
-        cout << test_count++ << " source:" << source << " destination:" << dest << "\n";
-        
-        elapsed.start();
-        Len resAStar = astar.query(source,dest);
-        elapsed.stop("AStar query");
-        elapsed.start();
-        Len resDijks = dijks.query(source,dest);
-        elapsed.stop("Dijkstra query");
-        //cout << "sample cost:" << COST << "\n";
-        cout << "astar:" << resAStar << " processed:" << astar.getProcessedCount() << " workset:" << astar.getWorksetCount() << "\n";
-        cout << "dijks:" << resDijks << " processed:" << dijks.getProcessedCount() << " workset:" << dijks.getWorksetCount() << "\n";
-        
-        if (resAStar != resDijks) {
-            printf(DISP "==" DISP "\n", resAStar, resDijks);
-        
-            //path = astar.getPath();
-            //std::for_each(path.begin(), path.end(), [](int& elem) {cout << elem << "->";});cout << "\n";
-
-            //path = dijks.getPath();
-            //std::for_each(path.begin(), path.end(), [](int& elem) {cout << elem << "->";});cout << "\n";
-            
-            for (int i = 0; i < COUNT; i++) {
-                cout << "\nPoint:" << i << "(" << xy[i].first << "," << xy[i].second << ")" << " adj:";
-                for (int j = 0; j < adj[F][i].size(); j++) {
-                    cout << " " << adj[F][i][j] << "(" << cost[F][i][j] << ")";
-                }
-            }
-            cout << "\n";
-            for (int i = 0; i < COUNT; i++) {
-                cout << "\nPoint:" << i << "(" << xy[i].first << "," << xy[i].second << ")" << " adj:";
-                for (int j = 0; j < adj[R][i].size(); j++) {
-                    cout << " " << adj[R][i][j] << "(" << cost[R][i][j] << ")";
-                }
-            }
-            cout << "\n";
-
-            break;
-        }
-        
-        if (astar.getProcessedCount() > dijks.getProcessedCount()) {
-            getchar();
-        }
-        //cout << "\n\n\n\n\n\n";
-        //getchar();
-    }
-}
-*/
